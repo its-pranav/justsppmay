40d6a98c2c033ede3ed8246afcd5e481c4668547
Pranav <102531883+its-pranav@users.noreply.github.com>
2022-12-11 19:07:24 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-STRING = getenv("AQAfYGeKaL746HaqXaGdkcXJnUySsoJT6-XvwuHln6YuZEwkPwRaUp0aImALhnpMXulyzydL23SoaLIJx5-hFOQWC6U3611erYkJr1R9eH2ynaSdc2aHH28MnV0cGyad76q40g5WBo5Jcw18uqBhzZl1VJLvH4uFyoZ-zyMmZqDOW3vDCq0SYCbqlipC9S28r9TCTyxgC8IYwkR1YsRz7SjKwzCyffvy6F6UGWjo4qJM4bJBvCf--Mz2DBGH-UV1uqt_fyPVGOUqcroBCIQ_fXdGAwmUfBRZXTnBWOo_rKLOSSdvxp6kvAY02KlUT3EF_dDE8m8rr39BzpmL7i_qQQFFAAAAAVk7-HgA
-")
+STRING = getenv("AQAfYGeKaL746HaqXaGdkcXJnUySsoJT6-XvwuHln6YuZEwkPwRaUp0aImALhnpMXulyzydL23SoaLIJx5-hFOQWC6U3611erYkJr1R9eH2ynaSdc2aHH28MnV0cGyad76q40g5WBo5Jcw18uqBhzZl1VJLvH4uFyoZ-zyMmZqDOW3vDCq0SYCbqlipC9S28r9TCTyxgC8IYwkR1YsRz7SjKwzCyffvy6F6UGWjo4qJM4bJBvCf--Mz2DBGH-UV1uqt_fyPVGOUqcroBCIQ_fXdGAwmUfBRZXTnBWOo_rKLOSSdvxp6kvAY02KlUT3EF_dDE8m8rr39BzpmL7i_qQQFFAAAAAVk7-HgA")
 STRING2 = getenv("STRING2")
 STRING3 = getenv("STRING3")
 STRING4 = getenv("STRING4")
@@ -14,8 +13,8 @@ STRING7 = getenv("STRING7")
 STRING8 = getenv("STRING8")
 STRING9 = getenv("STRING9")
 STRING10 = getenv("STRING10")
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
+API_ID = getenv("25343539")
+API_HASH = getenv("1283744fca7e8a1b8cf162c6fe08b87c")
 ALIVE_PIC = getenv("ALIVE_PIC")
 BIO_MESSAGE = getenv("BIO")
 SUDO = list(map(int, getenv("SUDO").split()))
